54204b62c5142a37159ace4dcc61b7938346b6a5
SadullaevTolkinjon <sadulayevtolik1997@gmail.com>
2023-05-08 03:34:14 +0500

    last commit

--- lib/presentation/widgets/error_page.dart
+++ lib/presentation/widgets/error_page.dart
@@ -1,16 +1,25 @@
 import 'package:flutter/widgets.dart';
 
 class ShowError extends StatelessWidget {
-  const ShowError({super.key,this.error});
-final error;
+  const ShowError({super.key, this.error});
+  final error;
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 16),
-      child: Column(
-        children: [
-          Text(error)
-        ],
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            const Text(
+              "Something went wrong",
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.w700),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 20),
+              child: Text(error),
+            )
+          ],
+        ),
       ),
     );
   }
